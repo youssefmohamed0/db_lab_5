ab0634b246e560202557facdff8f3c03ace3d8ca
youssefmohamed0 <youssefyoussef.m4@gmail.com>
2025-10-28 21:32:07 +0300

    minor modifications

--- creating_db.sql
+++ creating_db.sql
@@ -9,15 +9,15 @@ CREATE TABLE category (
     PRIMARY KEY (category_id)
 );
 CREATE TABLE publisher (
-    pub_id INT NOT NULL,
+    pub_id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     address VARCHAR(255),
     PRIMARY KEY (pub_id)
 );
 CREATE TABLE member (
-    member_id INT NOT NULL,
+    member_id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
-    address VARCHAR(255) NOT NULL,
+    address VARCHAR(255),
     join_date TIMESTAMP NOT NULL,
     PRIMARY KEY(member_id)
 );
@@ -25,7 +25,7 @@ CREATE TABLE book (
     book_id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(255) NOT NULL,
     price FLOAT(23) NOT NULL,
-    pub_id INT(255) NOT NULL,
+    pub_id INT NOT NULL,
     category_id INT NOT NULL,
     PRIMARY KEY (book_id),
     FOREIGN KEY (pub_id) REFERENCES publisher(pub_id),
@@ -34,9 +34,9 @@ CREATE TABLE book (
 CREATE TABLE borrowing_book (
     member_id INT NOT NULL,
     book_id INT NOT NULL,
-    borrow_date TIMESTAMP,
-    due_date TIMESTAMP,
-    return_date TIMESTAMP,
+    borrow_date TIMESTAMP NOT NULL,
+    due_date TIMESTAMP NOT NULL,
+    return_date TIMESTAMP NOT NULL,
     FOREIGN KEY (member_id) REFERENCES member(member_id),
     FOREIGN KEY (book_id) REFERENCES book(book_id)
 ); 
